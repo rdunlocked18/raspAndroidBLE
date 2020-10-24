5a7b6e90903bebf18889fcc97b35b26bc542f5cf
rdunlocked18 <rohitdaftari3@gmail.com>
2020-10-25 00:07:36 +0530

    Json Send Done , Logintime Valid

--- app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
+++ app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
@@ -1,57 +1,55 @@
 package de.rcoemcs.iotbase.Logins;
 
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.bluetooth.BluetoothAdapter;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.DataOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 
 import de.rcoemcs.iotbase.MainActivity;
-import de.rcoemcs.iotbase.R;
 import de.rcoemcs.iotbase.databinding.ActivityLoginScreenBinding;
 
 public class LoginScreen extends AppCompatActivity {
@@ -66,6 +64,7 @@ public class LoginScreen extends AppCompatActivity {
     private BiometricPrompt.PromptInfo promptInfo;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,7 +74,7 @@ public class LoginScreen extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-
+        handlePermissions();
 
 
         activityLoginScreenBinding.btnLoginGet.setOnClickListener(v -> {
@@ -133,6 +132,7 @@ public class LoginScreen extends AppCompatActivity {
         String password = Objects.requireNonNull(activityLoginScreenBinding.passwordInputLogin.getText()).toString().trim();
 
         String mainUsersGetUrl = "http://iotrest.herokuapp.com/api/iot?name=" + username + "&pwd=" + password;
+        String mainPostDeviceFetcherUrl = "http://iotrest.herokuapp.com/api/devicefetcher/";
         Log.d(TAG, "checkLogin: " + mainUsersGetUrl);
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
@@ -153,6 +153,7 @@ public class LoginScreen extends AppCompatActivity {
                                     //sending macid as static
                                     if (jsonObject.getString("userName").equals("varun")) {
                                         _MACID = "11:20:0A:BB:28:FC";
+
                                     } else if (jsonObject.getString("userName").equals("rohit")) {
                                         _MACID = "00:00:0A:BB:28:FC";
                                     } else if (jsonObject.getString("userName").equals("srihari")) {
@@ -161,11 +162,19 @@ public class LoginScreen extends AppCompatActivity {
                                         _MACID = "AB:0C:05:BF:3B:89";
                                     } else if (jsonObject.getString("userName").equals("pritesh")) {
                                         _MACID = "9C:F4:B5:65:14:C0";
+                                    }else  {
+                                        _MACID = "00:00:00:00:20";//default
                                     }
                                     //mac end
                                     Snackbar.make(activityLoginScreenBinding.getRoot(), "Valid User .. Getting Inside..", Snackbar.LENGTH_SHORT)
                                             .show();
-                                    jsonPostToDeviceFetcher();
+
+
+                                  PostOperation();
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(() -> startActivity(new Intent(LoginScreen.this, MainActivity.class)),1000);
+
+
 
                                 } else {
                                     Snackbar.make(activityLoginScreenBinding.getRoot(), "Ask Admins to Validate User", Snackbar.LENGTH_SHORT)
@@ -193,18 +202,13 @@ public class LoginScreen extends AppCompatActivity {
         requestQueue.add(jsonArrayRequest);
     }
 
-    private void jsonPostToDeviceFetcher() {
-
-
-        Log.d(TAG, "jsonPostToDeviceFetcher: " + currentDateTime());
-
-    }
-
     public String currentDateTime() {
         Calendar c = Calendar.getInstance();
         SimpleDateFormat dateformat = new SimpleDateFormat("hh:mm:ss");
         String datetime = dateformat.format(c.getTime());
         return datetime;
+
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.P)
@@ -228,4 +232,54 @@ public class LoginScreen extends AppCompatActivity {
             }
         }).check();
     }
+
+    public void PostOperation() {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "http://iotrest.herokuapp.com/api/devicefetcher";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("loginTime", currentDateTime());
+            jsonBody.put("macId", _MACID);
+            jsonBody.put("authId", _ObjectID);
+            final String mRequestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> Log.i("LOG_VOLLEY", response), new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+
+                        responseString = String.valueOf(response.statusCode);
+
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
