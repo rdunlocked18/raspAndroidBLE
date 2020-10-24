99509c57420f48e55ecce455f8f158045dd2d733
rdunlocked18 <rohitdaftari3@gmail.com>
2020-10-24 18:08:18 +0530

    Static MAC id Full Login Working

--- app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
+++ app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
@@ -3,9 +3,11 @@ package de.rcoemcs.iotbase.Logins;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -25,7 +27,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Objects;
 
 import de.rcoemcs.iotbase.MainActivity;
@@ -37,7 +43,8 @@ public class LoginScreen extends AppCompatActivity {
     private static final String TAG = "Login Screen ";
     ActivityLoginScreenBinding activityLoginScreenBinding;
     RequestQueue requestQueue;
-
+    String _ObjectID = "";
+    String _MACID = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,20 +74,69 @@ public class LoginScreen extends AppCompatActivity {
                 null,
                 response -> {
                     try {
-                        for (int i = 0; i < response.length(); i++) {
-                            JSONObject student = response.getJSONObject(i);
-                            String firstName = student.getString("userName");
-                            String lastName = student.getString("password");
-                            Toast.makeText(LoginScreen.this, "" + firstName, Toast.LENGTH_SHORT).show();
+                        if (response.getJSONObject(0) == null) {
+                            Snackbar.make(activityLoginScreenBinding.getRoot(), "Check Username or Password", Snackbar.LENGTH_SHORT)
+                                    .show();
+                        } else {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject jsonObject = response.getJSONObject(i);
+
+                                if (jsonObject.getBoolean("userValid")) {
+                                    _ObjectID = jsonObject.getString("_id");
+                                    //sending macid as static
+                                    if(jsonObject.getString("userName").equals("varun")){
+                                        _MACID = "11:20:0A:BB:28:FC";
+                                    }else  if(jsonObject.getString("userName").equals("rohit")){
+                                        _MACID = "00:00:0A:BB:28:FC";
+                                    }else  if(jsonObject.getString("userName").equals("srihari")){
+                                        _MACID = "BA:9E:1D:6E:93:10";
+                                    }else  if(jsonObject.getString("userName").equals("suraj")){
+                                        _MACID = "AB:0C:05:BF:3B:89";
+                                    }else  if(jsonObject.getString("userName").equals("pritesh")){
+                                        _MACID = "9C:F4:B5:65:14:C0";
+                                    }
+                                    //mac end
+                                    Snackbar.make(activityLoginScreenBinding.getRoot(), "Valid User .. Getting Inside..", Snackbar.LENGTH_SHORT)
+                                            .show();
+                                    jsonPostToDeviceFetcher();
+
+                                } else {
+                                    Snackbar.make(activityLoginScreenBinding.getRoot(), "Ask Admins to Validate User", Snackbar.LENGTH_SHORT)
+                                            .setAction("Retry", v -> checkLogin())
+                                            .show();
+                                }
+
+                            }
                         }
+
                     } catch (JSONException e) {
                         e.printStackTrace();
+                        Snackbar.make(activityLoginScreenBinding.getRoot(), "Check Username or Password", Snackbar.LENGTH_SHORT)
+                                .setAction("Retry", v -> checkLogin())
+                                .show();
                     }
+
                 },
                 error -> {
-                    Toast.makeText(LoginScreen.this, ""+error, Toast.LENGTH_SHORT).show();
+                    Snackbar.make(activityLoginScreenBinding.getRoot(), "Invalid User This App is For Valid Users Only", Snackbar.LENGTH_SHORT)
+                            .setAction("Retry", v -> checkLogin())
+                            .show();
                 }
         );
         requestQueue.add(jsonArrayRequest);
+    }
+
+    private void jsonPostToDeviceFetcher() {
+
+
+        Log.d(TAG, "jsonPostToDeviceFetcher: " + currentDateTime());
+
+    }
+
+    public String currentDateTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("hh:mm:ss");
+        String datetime = dateformat.format(c.getTime());
+        return datetime;
     }
 }
