7154dc560fcae10cc5abecb8531bad72568c3b9a
rdunlocked18 <rohitdaftari3@gmail.com>
2020-10-24 21:29:45 +0530

    FingerPrint Added , Callbacks Added

--- app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
+++ app/src/main/java/de/rcoemcs/iotbase/Logins/LoginScreen.java
@@ -1,15 +1,23 @@
 package de.rcoemcs.iotbase.Logins;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -22,6 +30,12 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,7 +46,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.Executor;
 
 import de.rcoemcs.iotbase.MainActivity;
 import de.rcoemcs.iotbase.R;
@@ -45,6 +61,11 @@ public class LoginScreen extends AppCompatActivity {
     RequestQueue requestQueue;
     String _ObjectID = "";
     String _MACID = "";
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,9 +75,55 @@ public class LoginScreen extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(getApplicationContext());
 
+
+
+
         activityLoginScreenBinding.btnLoginGet.setOnClickListener(v -> {
             checkLogin();
         });
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginScreen.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+                System.exit(0);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Not Recognised Please Retry",
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Please Authenticate Device Ownership!")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+
+        biometricPrompt.authenticate(promptInfo);
 
 
     }
@@ -84,15 +151,15 @@ public class LoginScreen extends AppCompatActivity {
                                 if (jsonObject.getBoolean("userValid")) {
                                     _ObjectID = jsonObject.getString("_id");
                                     //sending macid as static
-                                    if(jsonObject.getString("userName").equals("varun")){
+                                    if (jsonObject.getString("userName").equals("varun")) {
                                         _MACID = "11:20:0A:BB:28:FC";
-                                    }else  if(jsonObject.getString("userName").equals("rohit")){
+                                    } else if (jsonObject.getString("userName").equals("rohit")) {
                                         _MACID = "00:00:0A:BB:28:FC";
-                                    }else  if(jsonObject.getString("userName").equals("srihari")){
+                                    } else if (jsonObject.getString("userName").equals("srihari")) {
                                         _MACID = "BA:9E:1D:6E:93:10";
-                                    }else  if(jsonObject.getString("userName").equals("suraj")){
+                                    } else if (jsonObject.getString("userName").equals("suraj")) {
                                         _MACID = "AB:0C:05:BF:3B:89";
-                                    }else  if(jsonObject.getString("userName").equals("pritesh")){
+                                    } else if (jsonObject.getString("userName").equals("pritesh")) {
                                         _MACID = "9C:F4:B5:65:14:C0";
                                     }
                                     //mac end
@@ -138,5 +205,27 @@ public class LoginScreen extends AppCompatActivity {
         SimpleDateFormat dateformat = new SimpleDateFormat("hh:mm:ss");
         String datetime = dateformat.format(c.getTime());
         return datetime;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void handlePermissions() {
+        Dexter.withContext(this)
+                .withPermissions(
+                        Manifest.permission.BLUETOOTH,
+                        Manifest.permission.BLUETOOTH_ADMIN,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.FOREGROUND_SERVICE,
+                        Manifest.permission.BLUETOOTH_ADMIN
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                //Toast.makeText(LoginScreen.this, "Checked !", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+            }
+        }).check();
     }
 }
